93275aa0f598d509eab4ae9617870dec2d42afb5
Ricardo Soares de Lima <ricardo-sl@segplan.go.gov.br>
2018-01-25 17:42:22 -0200

    The Stack implementation, with bugs for now

---
+++ units/ustack.pas
@@ -0,0 +1,102 @@
+unit UStack;
+
+{$mode objfpc}{$H+}{$J-}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+type
+  PElement = ^TElement;
+  PPElement = ^PElement;
+  TElement = record
+    Next: PElement;
+    Data: Pointer;
+  end;
+
+  TPrintStackItem = procedure(Item: Pointer);
+
+
+
+  function CreateStack(Stack: PPElement): Boolean;
+  function DeleteStack(Stack: PPElement): Boolean;
+  function Push(Stack: PPElement; Data: Pointer): Boolean;
+  function Pop(Stack: PPElement; Data: PPointer): Boolean;
+  procedure PrintStack(Stack: PElement; PrintItem: TPrintStackItem);
+
+
+
+
+implementation
+
+function CreateStack(Stack: PPElement): Boolean;
+begin
+  Stack^ := Nil;
+  Result := True;
+end;
+
+function DeleteStack(Stack: PPElement): Boolean;
+var
+  CurrentElement: PElement;
+begin
+
+  CurrentElement := Stack^;
+
+  while Stack^ <> nil do
+  begin
+    Dispose(Stack^);
+    Stack^ := CurrentElement^.Next;
+  end;
+
+  Result := True;
+
+end;
+
+function Push(Stack: PPElement; Data: Pointer): Boolean;
+var
+  NewElement: PElement;
+begin
+  New(NewElement);
+  if NewElement = nil then Exit(False);
+
+  NewElement^.Data := Data;
+  NewElement^.Next := Stack^;
+  Stack^ := NewElement;
+
+  Result := True;
+
+
+end;
+
+function Pop(Stack: PPElement; Data: PPointer): Boolean;
+var
+  OldTop: PElement;
+begin
+
+  if Stack^ = Nil then Exit(False);
+
+  Data^ := Stack^^.Data;
+
+  OldTop := Stack^;
+
+  Stack^ := Stack^^.Next;
+
+  Dispose(OldTop);
+
+  Exit(True);
+
+
+end;
+
+procedure PrintStack(Stack: PElement; PrintItem: TPrintStackItem);
+begin
+  while Stack <> nil do
+  begin
+    PrintItem(Stack^.Data);
+    Stack := Stack^.Next;
+  end;
+end;
+
+end.
+
